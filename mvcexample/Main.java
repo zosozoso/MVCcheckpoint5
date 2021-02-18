f70e7e348d67f6387528b96761eb48b17feb85f1
Montavris <55845858+Montavris@users.noreply.github.com>
2021-02-18 15:37:07 +0000

    Add files via upload

--- mvcexample/Main.java
+++ mvcexample/Main.java
@@ -7,18 +7,17 @@ package mvcexample;
 // Launcher for application
 
 public class Main {
-   
-    public static void main(String args[]) {
-     
-        Model model = new Model();               // Create shared database
-        Controller1 c1 = new Controller1(model, "controller 1"); // Create first controller
-        Controller2 c2 = new Controller2(model, "controller 2"); // Create second controller
-        Controller2 c3 = new Controller2(model, "controller 3"); // Create second controller
-        Controller2 c4 = new Controller2(model, "controller 4"); // Create second controller
-        Controller2 c5 = new Controller2(model, "controller 5"); // Create second controller
-        Controller2 c6 = new Controller2(model, "controller 6"); // Create second controller
-        Controller2 c7 = new Controller2(model, "controller 7"); // Create second controller
-        Controller2 c8 = new Controller2(model, "controller 8 "); // Create second controller
-    } // main
-  
+
+	public static void main(String args[]) {
+
+		Model model = new Model(); // Create shared database
+		Controller1 c1 = new Controller1(model); // Create first controller
+		
+		Controller2 c21 = new Controller2(model, "Controller2-1", 200); // Create second controller, first instance
+		Controller2 c22 = new Controller2(model, "Controller2-2", 360); // Create second controller, second instance
+		Controller2 c23 = new Controller2(model, "Controller2-3", 520); // Create second controller, third instance
+		Controller3 c3 = new Controller3(model, "Controller3", 680); //Create third controller
+
+	} // main
+
 } // Main
