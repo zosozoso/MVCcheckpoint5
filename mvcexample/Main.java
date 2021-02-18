ab5be0a1de6b5d1abf98fcf1e4f0c950a7787577
tianfeng <1120995623@qq.com>
2021-02-18 20:58:03 +0000

    main

--- mvcexample/Main.java
+++ mvcexample/Main.java
@@ -8,16 +8,14 @@ package mvcexample;
 
 public class Main {
 
-	public static void main(String args[]) {
+    public static void main(String args[]) {
 
-		Model model = new Model(); // Create shared database
-		Controller1 c1 = new Controller1(model); // Create first controller
-		
-		Controller2 c21 = new Controller2(model, "Controller2-1", 200); // Create second controller, first instance
-		Controller2 c22 = new Controller2(model, "Controller2-2", 360); // Create second controller, second instance
-		Controller2 c23 = new Controller2(model, "Controller2-3", 520); // Create second controller, third instance
-		Controller3 c3 = new Controller3(model, "Controller3", 680); //Create third controller
+        Model model = new Model(); // Create shared database
+        Controller1 c1 = new Controller1(model); // Create first controller
+        Controller2 c2 = new Controller2(model, "Controller2", 200); // Create second controller
+        Controller2 c3 = new Controller2(model, "Controller3", 360); // Create second controller
+        Controller2 c4 = new Controller2(model, "Controller4", 520); // Create second controller
 
-	} // main
+    } // main
 
 } // Main
